6d2a815399bcc765d5d098f3619fde4f9eb54b93
PolygonFox <PolygonFox@users.noreply.github.com>
2019-01-24 16:45:50 +0100

    Menu

--- src/components/navigation/MainNavigation/index.js
+++ src/components/navigation/MainNavigation/index.js
@@ -1,15 +1,52 @@
 import React from 'react'
+import classnames from 'classnames'
 
 import './style.scss'
 
 export default class MainNavigation extends React.Component {
+
+  state = {
+    hasScrolled: false
+  }
+
+  componentWillMount() {
+    window.addEventListener('scroll', this._scrollCheck)
+    this._scrollCheck()
+
+    this.animationIn = React.createRef()
+    this.animationOut = React.createRef()
+  }
+
   render() {
     return <nav>
-      <div className='main-navigation'>
-        <a className='main-navigation__link main-navigation__link--left' href='#'>Skills</a>
+      <div className={classnames('main-navigation', this.state.hasScrolled && 'has-scrolled')}>
+        <a className='main-navigation__link main-navigation__link--left' href='#portfolio'><span>Portfolio</span></a>
         <h1 className='site-name'>PolygonFox</h1>
-        <a className='main-navigation__link main-navigation__link--right' href='#'>Art</a>
+        <a className='main-navigation__link main-navigation__link--right' href='#contact'><span>Contact</span></a>
+        <svg  viewBox='0 0 100 100'>
+          <polygon className="square" points="0 50, 50 0, 100 50, 50 100">
+            <animate ref={this.animationIn} attributeName="points" dur="200ms" to="0 50, 50 0, 100 50, 50 50" begin="none"  fill="freeze" />
+            <animate ref={this.animationOut} attributeName="points" dur="200ms" to="0 50, 50 0, 100 50, 50 100" begin="none"  fill="freeze" />
+          </polygon>
+        </svg>
       </div>
     </nav>
+  }
+  
+  _scrollCheck = () => {
+    const currentScrollPosition = window.scrollY
+    const currentHasScrolledState = this.state.hasScrolled
+    console.log(currentScrollPosition, currentHasScrolledState)
+    if(currentHasScrolledState === false && currentScrollPosition > 0) {
+      this.animationIn.current.beginElement()
+      this.setState({
+        hasScrolled: true
+      })
+    } else if(currentHasScrolledState === true && currentScrollPosition === 0) {
+      this.animationOut.current.beginElement()
+      this.setState({
+        hasScrolled: false
+      })
+    }
   }
 }
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -11,14 +11,30 @@ export default class App extends React.Component {
       <HeroArea />
 
       <div className='container'>
-        <div className='row my-1'>
+        <div className='row my-3 h100'>
           <div className='col-12'>
-            <h1>Welcome</h1>
+            <h2>Introduction</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare condimentum elit eget lacinia. Ut lectus dolor, luctus ut molestie id, aliquam non dolor. Nam purus tellus, viverra a venenatis a, tempus at velit. Suspendisse sed augue lorem. Proin ut est erat. Aliquam quis fringilla nisi. Praesent orci velit, fermentum venenatis placerat a, semper quis dolor. Suspendisse ac lectus odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer imperdiet enim a placerat tempus. Nam ultrices, justo eget mattis pharetra, lacus odio laoreet ipsum, nec pellentesque odio orci suscipit neque. Mauris accumsan sed velit ac faucibus. Pellentesque id felis non dolor dignissim ullamcorper id eu ligula. Donec sed sem ac erat venenatis euismod.</p>
+            <p>Nam est sem, fermentum sit amet ultrices ut, vehicula quis tellus. Nulla dignissim sagittis augue iaculis vulputate. Aliquam efficitur volutpat risus scelerisque vestibulum. Maecenas vulputate aliquet lectus ut varius. Mauris quam lorem, egestas sagittis orci suscipit, tempus auctor diam. Aliquam nec nunc vel turpis mollis consequat eu eget eros. Vestibulum finibus mi erat, ut ultricies libero bibendum a. Fusce ultricies est at mattis rutrum. Donec facilisis tortor vel sagittis pretium. Sed volutpat neque at ullamcorper euismod. Cras rhoncus sagittis consectetur. Nam eget purus ut enim elementum dapibus. Nam laoreet facilisis tristique.</p>
+          </div>
+        </div>
+        <div className='row my-3 h100'>
+          <div className='col-12'>
+            <h2>Skills</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare condimentum elit eget lacinia. Ut lectus dolor, luctus ut molestie id, aliquam non dolor. Nam purus tellus, viverra a venenatis a, tempus at velit. Suspendisse sed augue lorem. Proin ut est erat. Aliquam quis fringilla nisi. Praesent orci velit, fermentum venenatis placerat a, semper quis dolor. Suspendisse ac lectus odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer imperdiet enim a placerat tempus. Nam ultrices, justo eget mattis pharetra, lacus odio laoreet ipsum, nec pellentesque odio orci suscipit neque. Mauris accumsan sed velit ac faucibus. Pellentesque id felis non dolor dignissim ullamcorper id eu ligula. Donec sed sem ac erat venenatis euismod.</p>
+            <p>Nam est sem, fermentum sit amet ultrices ut, vehicula quis tellus. Nulla dignissim sagittis augue iaculis vulputate. Aliquam efficitur volutpat risus scelerisque vestibulum. Maecenas vulputate aliquet lectus ut varius. Mauris quam lorem, egestas sagittis orci suscipit, tempus auctor diam. Aliquam nec nunc vel turpis mollis consequat eu eget eros. Vestibulum finibus mi erat, ut ultricies libero bibendum a. Fusce ultricies est at mattis rutrum. Donec facilisis tortor vel sagittis pretium. Sed volutpat neque at ullamcorper euismod. Cras rhoncus sagittis consectetur. Nam eget purus ut enim elementum dapibus. Nam laoreet facilisis tristique.</p>
+          </div>
+        </div>
+
+        <div className='row my-3 h100'>
+          <div className='col-12'>
+            <h2 id="portfolio">Portfolio</h2>
             <h2>Welcome</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ornare condimentum elit eget lacinia. Ut lectus dolor, luctus ut molestie id, aliquam non dolor. Nam purus tellus, viverra a venenatis a, tempus at velit. Suspendisse sed augue lorem. Proin ut est erat. Aliquam quis fringilla nisi. Praesent orci velit, fermentum venenatis placerat a, semper quis dolor. Suspendisse ac lectus odio. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer imperdiet enim a placerat tempus. Nam ultrices, justo eget mattis pharetra, lacus odio laoreet ipsum, nec pellentesque odio orci suscipit neque. Mauris accumsan sed velit ac faucibus. Pellentesque id felis non dolor dignissim ullamcorper id eu ligula. Donec sed sem ac erat venenatis euismod.</p>
+            <p>Nam est sem, fermentum sit amet ultrices ut, vehicula quis tellus. Nulla dignissim sagittis augue iaculis vulputate. Aliquam efficitur volutpat risus scelerisque vestibulum. Maecenas vulputate aliquet lectus ut varius. Mauris quam lorem, egestas sagittis orci suscipit, tempus auctor diam. Aliquam nec nunc vel turpis mollis consequat eu eget eros. Vestibulum finibus mi erat, ut ultricies libero bibendum a. Fusce ultricies est at mattis rutrum. Donec facilisis tortor vel sagittis pretium. Sed volutpat neque at ullamcorper euismod. Cras rhoncus sagittis consectetur. Nam eget purus ut enim elementum dapibus. Nam laoreet facilisis tristique.</p>
           </div>
         </div>
       </div>
-
     </div>
   }
 }
